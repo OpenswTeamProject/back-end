999e3ec189d17f69b73e1e6a3d94a846e1e42b73
chyun7114 <chyun7114@naver.com>
2024-11-20 13:45:27 +0900

    fix:csv

--- column.py
+++ column.py
@@ -1,20 +1,23 @@
 import pandas as pd
+from streamlit import columns
 
 # 엑셀 파일 읽기
-file_path = r"C:\Users\choho\Desktop\back-end\back-end\공공자전거 대여소 정보(24.6월 기준).csv"
+file_path = r"공공자전거 대여소 정보(24.6월 기준).csv"
 df = pd.read_csv(file_path, header=0)  # 첫 번째 행을 컬럼 이름으로 사용
 
 # 데이터프레임의 컬럼 확인
 print("컬럼 이름 확인:", df.columns)
+
+
 
 # null 값을 0으로 대체
 df = df.fillna(0)
 
 # 새 컬럼 추가 (기존 컬럼 유지)
 df['거치대수 합계'] = df['거치대수1'] + df['거치대수2']  # 거치대수1과 거치대수2 컬럼 이름은 확인 후 수정
-
+df.drop(columns = ['거치대수1', '거치대수2'], inplace=True)
 # CSV 파일로 저장
-output_csv_path = r"C:\Users\choho\Desktop\back-end\back-end\공공자전거 대여소 정보(24.6월 기준).csv"
+output_csv_path = r"공공자전거 대여소 정보(24.6월 기준)-1.csv"
 df.to_csv(output_csv_path, index=False, encoding='utf-8-sig')
 
 print(f"CSV 파일이 성공적으로 저장되었습니다: {output_csv_path}")
